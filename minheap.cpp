b4e62092172bfffee7f378a4568e02d39da19e17
tom-sb <fvillanuevanut@gmail.com>
2017-10-24 18:21:55 -0500

    Add files via upload

---
+++ minheap.cpp
@@ -0,0 +1,144 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+ 
+void swap(int* x, int* y){
+	int temp = *x;
+	*x = *y;
+	*y = temp;
+}
+
+//template <class T>
+class MinHeap{
+		int *pArray;
+		int capacity;
+		int heap_size;
+	public:
+		MinHeap(int capacity);
+		void Heapify(int i);
+		int parent(int i);
+		int left(int i);
+		int right(int i);
+		int extractMin();
+		void decereaseKey(int i, int new_val);
+		int getMin();
+		void deleteKey(int i);
+		void insertKey(int k);
+};
+
+//template <class T>
+MinHeap::MinHeap(int tam){
+	heap_size = 0;
+	capacity = tam;
+	pArray = new int[tam];
+}
+
+//template <class T>
+int MinHeap::parent(int i){
+	return ((i-1)/2);
+}
+
+//template <class T>
+int MinHeap::left(int i){
+	return (2*i + 1);
+}
+
+//template <class T>
+int MinHeap::right(int i){
+	return (2*i + 2);
+}
+
+//template <class T>
+void MinHeap::Heapify(int i){
+	int l = left(i);
+	int r = right(i);
+	int smallest = i; 
+	if (l < heap_size && pArray[l] < pArray[i])
+		smallest =l;
+	if (r < heap_size && pArray[r] < pArray[smallest])
+		smallest = r;
+	if (smallest != i){
+		swap(&pArray[i], &pArray[smallest]);
+		Heapify(smallest);
+	}
+
+	/*int H = i;
+	if (pArray[i] < pArray[left(pArray[i])])
+		H = pArray[left(pArray[i])];
+	if (H < pArray[right(pArray[i])])
+		H = pArray[right(pArray[i])];
+	if (H == i) return;
+	swap(pArray[i], pArray[H]);
+	Heapify(H);*/
+	
+}
+
+//template <class T>
+int MinHeap::extractMin(){
+	if (heap_size <= 0)
+		return INT_MAX;
+	if (heap_size == 1){
+		heap_size--;
+		return pArray[0];
+	}
+
+	int root = pArray[0];
+	pArray[0] = pArray[heap_size-1];
+	heap_size--;
+	Heapify(0);
+
+	return root;
+}
+
+//template <class T>
+void MinHeap::decereaseKey(int i, int new_val){
+	pArray[i] = new_val;
+	while(i != 0 && pArray[parent(i)] > pArray[i]){
+		swap(&pArray[i], &pArray[parent(i)]);
+		i = parent(i);
+	}
+}
+
+//template <class T>
+int MinHeap::getMin(){
+	return pArray[0];
+}
+
+//template <class T>
+void MinHeap::deleteKey(int i){
+	decereaseKey(i, INT_MIN);
+	extractMin();
+}
+
+//template <class T>
+void MinHeap::insertKey(int k){
+	if (heap_size == capacity){
+		cout<<"no se puede insertar key"<<endl;
+		return;
+	}
+	
+	heap_size++;
+	int i = heap_size -1;
+	pArray[i] = k;
+
+	while (i != 0 && pArray[parent(i)] > pArray[i]){
+		swap(&pArray[i], &pArray[parent(i)]);
+		i = parent(i);
+	}
+}
+
+int main(){
+	MinHeap minh1(11);
+	minh1.insertKey(3);
+	minh1.insertKey(2);
+	minh1.deleteKey(1);
+	minh1.insertKey(15);
+	minh1.insertKey(5);
+	minh1.insertKey(4);
+	minh1.insertKey(45);
+	cout<<minh1.extractMin()<< " ";
+	cout<<minh1.getMin()<< " ";
+	minh1.decereaseKey(2,1);
+	cout<<minh1.getMin();
+	return 0;
+}
